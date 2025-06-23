af0cc92b92d84683f51209e88fbb3f8532a046c0
hayeon <hayeonkimmie@gmail.com>
2025-06-23 18:13:05 +0900

    fix : 관리자페이지 배너 경로 수정

--- src/main/java/com/dev/moyering/config/WebConfig.java
+++ src/main/java/com/dev/moyering/config/WebConfig.java
@@ -2,19 +2,16 @@ package com.dev.moyering.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        // 리액트 서버로 요청
-        registry.addMapping("/*") // 루트 이하의 모든 것
-                .allowedOrigins("http://localhost:5173")
-                .allowedMethods("GET", "POST", "PUT", "DELETE");
-
-
-        WebMvcConfigurer.super.addCorsMappings(registry);
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // iupload 폴더의 파일들을 정적 파일로 서빙
+        registry.addResourceHandler("/iupload/**")
+                .addResourceLocations("file:iupload/");
     }
 }
