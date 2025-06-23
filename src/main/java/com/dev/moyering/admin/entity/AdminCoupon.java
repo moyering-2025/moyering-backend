3f377a48b7e624868e8e231e95bdc87f932ac034
hayeon <hayeonkimmie@gmail.com>
2025-06-23 18:14:16 +0900

    add : 관리자페이지 coupon 엔티티&dto 추가

--- src/main/java/com/dev/moyering/admin/entity/AdminCoupon.java
+++ src/main/java/com/dev/moyering/admin/entity/AdminCoupon.java
@@ -2,27 +2,23 @@ package com.dev.moyering.admin.entity;
 
 import java.time.LocalDateTime;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import com.dev.moyering.admin.dto.AdminNoticeDto;
+import com.dev.moyering.host.entity.ClassCalendar;
+import lombok.*;
 
 @Entity
-@Table(name = "admin_coupon")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
+@ToString
+@Table(name="coupon")
 public class AdminCoupon {
     @Id @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Integer couponId;
+
+    @Column(nullable = false)
+    private String couponType;
 
     @Column(nullable = false)
     private String discountType; // 비율이면 'RT', 금액할인이면 'AMT'
@@ -31,6 +27,7 @@ public class AdminCoupon {
     private Integer discount; // 예: 10 (%), 5000 (금액)
 
     private LocalDateTime validFrom; //쿠폰 시작일
+
     private LocalDateTime validUntil; //쿠폰 종료일
 
     @Column(nullable = false)
@@ -38,4 +35,38 @@ public class AdminCoupon {
 
     @Column(nullable = false)
     private String couponName;
+
+    @ManyToOne
+    @JoinColumn(name = "calendarId")
+    private ClassCalendar calendar;
+
+    @Builder
+    public AdminCoupon(Integer couponId, String couponType, String discountType, Integer discount, LocalDateTime validFrom, LocalDateTime validUntil, LocalDateTime createdAt, String couponName, ClassCalendar calendar) {
+        this.couponId = couponId;
+        this.couponType = couponType;
+        this.discountType = discountType;
+        this.discount = discount;
+        this.validFrom = validFrom;
+        this.validUntil = validUntil;
+        this.createdAt = createdAt;
+        this.couponName = couponName;
+        this.calendar = calendar;
+    }
+
+    // 엔티티 -> toDto
+    public AdminCoupon toEntity() {
+        return new AdminCoupon(
+                this.couponId,
+                this.couponType,
+                this.discountType,
+                this.discount,
+                this.validFrom,
+                this.validUntil,
+                this.createdAt,
+                this.couponName,
+                this.calendar
+        );
+    }
 }
+
+
