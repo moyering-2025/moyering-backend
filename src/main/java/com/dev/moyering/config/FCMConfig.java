7637aab89ef7d4c0304d096a489e030629a26ed9
hayeon <hayeonkimmie@gmail.com>
2025-06-26 18:14:14 +0900

    add : 알람 Fcm에러 주석처리

--- src/main/java/com/dev/moyering/config/FCMConfig.java
+++ src/main/java/com/dev/moyering/config/FCMConfig.java
@@ -1,41 +1,41 @@
-package com.dev.moyering.config;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.List;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
-
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
-import com.google.firebase.messaging.FirebaseMessaging;
-
-@Configuration
-public class FCMConfig {
-	
-	@Bean
-	FirebaseMessaging firebaseMessaging() throws IOException {
-		ClassPathResource resource = new ClassPathResource("firebase/moyeoling-firebase-adminsdk-fbsvc-1befad9506.json");
-		InputStream refreshToken = resource.getInputStream();
-		FirebaseApp firebaseApp = null;
-		List<FirebaseApp> firebaseAppList = FirebaseApp.getApps();
-		
-		if(firebaseAppList != null && !firebaseAppList.isEmpty()) {
-			for(FirebaseApp app : firebaseAppList) {
-				if(app.getName().equals(FirebaseApp.DEFAULT_APP_NAME)) {
-					firebaseApp = app;
-					break;
-				}
-			}
-		} else {
-			FirebaseOptions options = FirebaseOptions.builder()
-					.setCredentials(GoogleCredentials.fromStream(refreshToken))
-					.build();
-			firebaseApp = FirebaseApp.initializeApp(options);
-		}
-		return FirebaseMessaging.getInstance(firebaseApp);
-	}
-}
+//package com.dev.moyering.config;
+//
+//import java.io.IOException;
+//import java.io.InputStream;
+//import java.util.List;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.core.io.ClassPathResource;
+//
+//import com.google.auth.oauth2.GoogleCredentials;
+//import com.google.firebase.FirebaseApp;
+//import com.google.firebase.FirebaseOptions;
+//import com.google.firebase.messaging.FirebaseMessaging;
+//
+//@Configuration
+//public class FCMConfig {
+//
+//	@Bean
+//	FirebaseMessaging firebaseMessaging() throws IOException {
+//		ClassPathResource resource = new ClassPathResource("firebase/moyeoling-firebase-adminsdk-fbsvc-1befad9506.json");
+//		InputStream refreshToken = resource.getInputStream();
+//		FirebaseApp firebaseApp = null;
+//		List<FirebaseApp> firebaseAppList = FirebaseApp.getApps();
+//
+//		if(firebaseAppList != null && !firebaseAppList.isEmpty()) {
+//			for(FirebaseApp app : firebaseAppList) {
+//				if(app.getName().equals(FirebaseApp.DEFAULT_APP_NAME)) {
+//					firebaseApp = app;
+//					break;
+//				}
+//			}
+//		} else {
+//			FirebaseOptions options = FirebaseOptions.builder()
+//					.setCredentials(GoogleCredentials.fromStream(refreshToken))
+//					.build();
+//			firebaseApp = FirebaseApp.initializeApp(options);
+//		}
+//		return FirebaseMessaging.getInstance(firebaseApp);
+//	}
+//}
