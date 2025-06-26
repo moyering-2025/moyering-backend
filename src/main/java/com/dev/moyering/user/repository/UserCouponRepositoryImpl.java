47c654a70776ef8ef125f92f91b3b7d70ef88d72
osk0521 <sangkyung21@naver.com>
2025-06-26 10:07:54 +0900

    파일 위치 에러 수정

--- src/main/java/com/dev/moyering/user/repository/UserCouponRepositoryImpl.java
+++
@@ -1,7 +0,0 @@
-package com.dev.moyering.user.repository;
-
-import com.dev.moyering.host.entity.ClassCoupon;
-import com.dev.moyering.user.entity.User;
-
-public class UserCouponRepositoryImpl implements UserCouponRepositoryCustom {
-}
