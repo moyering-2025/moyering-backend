99e2b9b46db6c26ab73fe3ac581e27d983520e69
김한재 <128352110+kimhanjae123@users.noreply.github.com>
2025-07-04 17:09:13 +0900

    강사 학생 서치 페이지 완성

--- src/main/java/com/dev/moyering/host/dto/StudentSearchRequestDto.java
+++ src/main/java/com/dev/moyering/host/dto/StudentSearchRequestDto.java
@@ -4,8 +4,8 @@ import lombok.Data;
 
 @Data
 public class StudentSearchRequestDto {
-
 	private Integer hostId;
+	private Integer userId;
 	private int page = 1;
 	private int size = 10;
 	private String keyword="";
--- src/main/java/com/dev/moyering/host/repository/ClassRegistRepositoryCustom.java
+++ src/main/java/com/dev/moyering/host/repository/ClassRegistRepositoryCustom.java
@@ -2,9 +2,12 @@ package com.dev.moyering.host.repository;
 
 import java.util.List;
 
+import com.dev.moyering.host.dto.CalendarUserDto;
 import com.dev.moyering.host.entity.ClassRegist;
 
 public interface ClassRegistRepositoryCustom {
 	List<ClassRegist> findByCalendar_HostClass_Host_HostId(Integer hostId);
+	
+	List<CalendarUserDto> findByStudentClass(Integer hostId,Integer userId);
 
 }
