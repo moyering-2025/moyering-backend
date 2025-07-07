61f699a564c16d8717075b33890f1ed8407ea0b3
s0ycraft <s0ycr4ft@gmail.com>
2025-07-07 14:12:11 +0900

    클래스 상세 추천 클래스

---
+++ src/main/java/com/dev/moyering/classring/dto/UserPaymentHistoryDto.java
@@ -0,0 +1,16 @@
+//package com.dev.moyering.classring.dto;
+//
+//import lombok.Setter;
+//import lombok.AllArgsConstructor;
+//import lombok.Builder;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//
+//@Getter
+//@Setter
+//@Builder
+//@AllArgsConstructor
+//@NoArgsConstructor
+//public class UserPaymentHistoryDto {
+//
+//}
--- src/main/java/com/dev/moyering/host/repository/ClassRegistRepository.java
+++ src/main/java/com/dev/moyering/host/repository/ClassRegistRepository.java
@@ -4,10 +4,12 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.dev.moyering.host.dto.ClassRegistDto;
 import com.dev.moyering.host.entity.ClassRegist;
 
 public interface ClassRegistRepository extends JpaRepository<ClassRegist, Integer>,ClassRegistRepositoryCustom {
 	List<ClassRegist> findByClassCalendarCalendarId(Integer calendarId)throws Exception;
 	ClassRegist findByUserUserId(Integer userInd)throws Exception;
+	List<ClassRegist> findAllByUser_UserId(Integer userId) throws Exception;
 
 }
