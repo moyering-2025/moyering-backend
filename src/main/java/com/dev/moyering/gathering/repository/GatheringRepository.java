8dbf1b4259f76e85334e34413defd678c92d3313
osk0521 <sangkyung21@naver.com>
2025-07-09 09:25:51 +0900

    스캐줄러 추가

--- src/main/java/com/dev/moyering/gathering/repository/GatheringRepository.java
+++ src/main/java/com/dev/moyering/gathering/repository/GatheringRepository.java
@@ -1,11 +1,15 @@
 package com.dev.moyering.gathering.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.dev.moyering.gathering.dto.GatheringDto;
 import com.dev.moyering.gathering.entity.Gathering;
-
+import java.sql.Timestamp;
+import java.util.List;
 public interface GatheringRepository extends JpaRepository<Gathering, Integer>, GatheringRepositoryCustom {
-
+	@Query("SELECT g FROM Gathering g WHERE g.applyDeadline < :currentTime AND g.canceled = false")
+	List<Gathering> findExpiredGatherings(@Param("currentTime") Timestamp currentTime);
 	
 }
